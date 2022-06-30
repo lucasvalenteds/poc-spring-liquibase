676ab3477db91d98e4885738959b90989ac76ec8
Lucas <lucasvalenteds@gmail.com>
2022-06-30 19:12:59 -0300

    Configuring Liquibase Gradle plugin
    Now we can rollback changes and execute commands not autoconfigured by
    Spring Boot via Command Line Interface (CLI).
    
    The configuration is the same as the one defined in
    `application.properties` file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import java.util.Properties
+
 plugins {
     id("org.springframework.boot") version "3.0.0-M3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.liquibase.gradle") version "2.1.1"
     java
 }
 
@@ -39,6 +42,12 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.assertj:assertj-core")
+
+    liquibaseRuntime("org.liquibase:liquibase-core")
+    liquibaseRuntime("org.liquibase:liquibase-groovy-dsl:3.0.2")
+    liquibaseRuntime("info.picocli:picocli:4.6.3")
+    liquibaseRuntime("org.postgresql:postgresql")
+    liquibaseRuntime("org.yaml:snakeyaml")
 }
 
 dependencyManagement {
@@ -49,4 +58,24 @@ dependencyManagement {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+liquibase {
+    val resourcesPath = "src/main/resources"
+    val applicationPropertiesFile = file(resourcesPath).resolve("application.properties")
+    val applicationProperties = Properties()
+    applicationProperties.load(applicationPropertiesFile.reader())
+
+    val activity = "main"
+    activities.register(activity) {
+        this.arguments = mapOf(
+            "logLevel" to "info",
+            "url" to applicationProperties["spring.datasource.url"],
+            "username" to applicationProperties["spring.datasource.username"],
+            "password" to applicationProperties["spring.datasource.password"],
+            "changeLogFile" to applicationProperties["spring.liquibase.change-log"].toString()
+                .replace("classpath:", "$resourcesPath/")
+        )
+        runList = activity
+    }
 }
