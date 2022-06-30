e558407f9d2cceaf62add2cb064ea232e1f1337c
Lucas <lucasvalenteds@gmail.com>
2022-06-30 00:22:44 -0300

    Inserting countries via database migration
    Also defining the corresponding rollback script.

---
+++ src/main/resources/db/changelog/changes/003-insert-countries-rollback.sql
@@ -0,0 +1,5 @@
+delete
+from COUNTRY
+where (NAME = 'United States' and CODE = 'US')
+   or (NAME = 'Canada' and CODE = 'CA')
+   or (NAME = 'Mexico' and CODE = 'MX');
---
+++ src/main/resources/db/changelog/changes/003-insert-countries.sql
@@ -0,0 +1,4 @@
+insert into COUNTRY (COUNTRY_ID, NAME, CODE)
+values (gen_random_uuid(), 'United States', 'US'),
+       (gen_random_uuid(), 'Canada', 'CA'),
+       (gen_random_uuid(), 'Mexico', 'MX')
